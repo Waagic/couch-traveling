c00432ddc8a93cce18e03fcd141594fa8059f1d4
galgros <galgros@yahoo.fr>
2020-04-29 23:39:50 +0200

    fix-bug

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,6 +21,6 @@ class HomeController extends AbstractController
      */
     public function index()
     {
-        return $this->twig->render('Home/show.html.twig');
+        return $this->twig->render('Home/index.html.twig');
     }
 }
