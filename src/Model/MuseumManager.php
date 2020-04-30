c2b10f238340571531afa49620c960909b285075
galgros <galgros@yahoo.fr>
2020-04-30 07:02:22 +0200

    add real data into museum page

---
+++ src/Model/MuseumManager.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Model;
+
+/**
+ *
+ */
+class MuseumManager extends AbstractManager
+{
+    /**
+     *
+     */
+    const TABLE = 'museum';
+
+    /**
+     *  Initializes this class.
+     */
+    public function __construct()
+    {
+        parent::__construct(self::TABLE);
+    }
+
+    public function selectByCountry($id)
+    {
+        //prepared request
+        $query = "SELECT * FROM " . self::TABLE . " m JOIN country c ON m.idCountry=c.id WHERE c.id=:id";
+        $statement = $this->pdo->prepare($query);
+        $statement->bindValue(':id', $id, \PDO::PARAM_INT);
+        $statement->execute();
+
+        return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
+}
